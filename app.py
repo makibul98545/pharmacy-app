f53db9d4b7fc66b705c166e44ec05acbdb7ee1da
Makibul Hussain <makibul89197@gmail.com>
2026-04-17 17:13:03 +0530

    final fix: flask init corrected

--- app.py
+++ app.py
@@ -12,11 +12,7 @@ import sys
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-app = Flask(
-    __name__,
-    template_folder=os.path.join(BASE_DIR, "templates"),
-    static_folder=os.path.join(BASE_DIR, "static")
-)
+app = Flask(__name__)
 CORS(app)    
 
 os.makedirs(os.path.join(BASE_DIR, 'database'), exist_ok=True)
